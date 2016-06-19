1b80919149924d31c2a69715e1631ce4912255b1
root <jainamritanshu@gmail.com>
2016-06-19 14:59:54 +0530

    j

--- app/models/user.js
+++ app/models/user.js
@@ -20,6 +20,8 @@ var UserSchema = new Schema({
   post_id: {type: Schema.Types.ObjectId, ref: 'PostSchema'}
 });
 
+var user = mongoose.model('User', UserSchema);
+
 
 /**var ashu = new UserSchema({
 	name: 'ashu jain',
@@ -38,31 +40,38 @@ var PostSchema = new Schema({
 	name: {type:String, default: ''},
 	content: {type:String, default: ''},
 	author: {type:String, default:''},
-	pub_date: {type: Date, default: 'Date.now'},  
+  	pub_date: {type:Date, required:true, default: Date.now},
 	comment_id: {type: Schema.Types.ObjectId, ref: 'CommentSchema'},
 	post_cat_id: {type: Schema.Types.ObjectId, ref: 'PostCatSchema'}
-})
+});
+var post = mongoose.model('Post', PostSchema);
 
 var CommentSchema = new Schema({
 	content: {type:String, default: ''},
 	post_id: {type: Schema.Types.ObjectId, ref: 'PostSchema'}
-})	
+});
+
+var comment = mongoose.model('Comment', CommentSchema);	
 
 var PostCatSchema = new Schema({
 	name: {type:String, default: ''}
-})
+});
+
+var postcat = mongoose.model('PostCat', PostCatSchema);
 
 var AlbumSchema = new Schema({
 	pub_date: {type: Date, default: 'Date.now'},
 	img_id: {type: Schema.Types.ObjectId, ref: 'ImageSchema'},
 	name: {type: String, default: ''}
-})
+});
+var album = mongoose.model('Album', AlbumSchema);
 
 
 var ImageSchema = new Schema({
 	image: {type: Image},
-	name: {type: String, default: ''},
-	album = {type: Schema.Types.ObjectId, ref: 'AlbumSchema'}
-}) 
+	name: {type: String, default: ''}
+}) ;
 
-module.exports = mongoose.model({('User', UserSchema)}, {('Post', PostSchema)}, {('PostCat', PostCatSchema)}, {('Comment', CommentSchema)}, {('Album', AlbumSchema)}, {('Image', ImageSchema)});
+var image = mongoose.model('Image', ImageSchema);
+
+module.exports = ({'user': user}, {'post': post}, {'postcat': postcat}, {'comment': comment}, {'album': album}, {'image': image});
